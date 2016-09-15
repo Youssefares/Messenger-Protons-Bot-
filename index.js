684bd4b2d92a70c7d072c943c82f14888624bd3f
Youssefares <Youssefares@ymail.com>
2016-09-15 20:14:32 +0200

    is it fixed?

--- index.js
+++ index.js
@@ -58,14 +58,14 @@ function receivedMessage(event){
 	  		break;
 
 	  		default: //echo
-	  		sendTextMessage(senderID, {text: "did you just say " + event.message.text+"?"});
+	  		sendMessage(senderID, {text: "did you just say " + event.message.text+"?"});
         
 	  	}
 	  }
 }
 
 // sends text messages
-function sendTextMessage(recipientId, message) {
+function sendMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
@@ -83,28 +83,6 @@ function sendTextMessage(recipientId, message) {
     });
 };
 
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });  
-}
-
 
 // sends structured message with a generic template
 function sendGenericMessage(recipientId) {
@@ -117,39 +95,29 @@ function sendGenericMessage(recipientId) {
         type: "template",
         payload: {
           template_type: "generic",
-          elements: [{
+          elements: [
+          {
             title: "rift",
             subtitle: "Next-generation virtual reality",
             item_url: "https://www.oculus.com/en-us/rift/",               
             image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
+            buttons: [
+            {
               type: "web_url",
               url: "https://www.oculus.com/en-us/rift/",
               title: "Open Web URL"
-            }, {
+            }, 
+            {
               type: "postback",
               title: "Call Postback",
               payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
+            }
+            ]
           }]
         }
       }
     }
   };  
 
-  callSendAPI(messageData);
+  sendMessage(messageData);
 }
