989d0a23e396d5bf36a4741d06c9ac9096077bea
Youssefares <Youssefares@ymail.com>
2017-01-19 23:09:03 +0200

    added python error help function & modified testing accordingly & tested

---
+++ helpers/python-help.js
@@ -0,0 +1,44 @@
+'use strict'
+
+//matches cryptic python error messages & returns a simpler sth about what maybe wrong.
+function help(lines){
+  /*
+  ____________________________________________________
+  Format:
+  "error message"
+  "result" = regex matched with "lines"
+  if matched return proper response & captured groups
+  ____________________________________________________
+  */
+
+
+  /*
+  Traceback (most recent call last):
+  File "python", line 2
+    if sth = sth:
+           ^
+  SyntaxError: invalid syntax
+  */
+
+  var result = /(if|while)\s+?(\w+)\s*=\s*(.+)\s*?:\s+.*\s+SyntaxError: invalid syntax/.exec(lines)
+  if(result){
+    let response = String.raw`Looks like you forgot to use "==" to compare "${result[2]}" and "${result[3]}" inside your ${result[1]} condition.` +
+    '\nRemember a single = sign means assignment:\nx == 1 is a literal of type bool that is either true or false.\nx = 1 is a statement that puts 1 inside of x.'
+
+    return { response: response, groups: groups(result)}
+  }
+
+  //no matches?
+  return null
+}
+
+//extracts & returns captured groups only from the re.exec() returned value
+function groups(result){
+  result.shift()
+  delete result['index']
+  delete result['input']
+  return result
+}
+
+
+module.exports = help
--- tests/python-help-test.js
+++ tests/python-help-test.js
@@ -2,22 +2,38 @@
 
 let chai = require('chai')
 let expect = chai.expect
-// var help = require('../helpers/python-help')
-
-// help('if   ds3   =   "fdsfsd":\n           ^\nSyntaxError: invalid syntax')
-// help("Traceback (most recent call last):\n  File \"python\", line 2\n    if variable = 6:\n             ^\n  SyntaxError: invalid syntax")
-// help("Traceback (most recent call last):\n  File \"python\", line 2\n    if    variable    =         6:\n                   ^\nSyntaxError: invalid syntax")
-//
+let help = require('../helpers/python-help')
 
 describe('helpers/python-help', function(){
+  /*
+  ________________________________
+  Format:
+  error message
+
+  -it('description')
+  -function call
+  -tests
+  _________________________________
+  */
+
+
+  /*
+  Traceback (most recent call last):
+  File "python", line 2
+    if sth = sth:
+           ^
+  SyntaxError: invalid syntax
+  */
   it('exec(regex) should not return null & should capture the two values being compared', function(){
-
-    //regex matching
-    var regex = /if\s+?(\w+)\s*=\s*(.+)\s*?:\s+.*\s+SyntaxError: invalid syntax/
-    var data = regex.exec("Traceback (most recent call last):\n  File \"python\", line 2\n    if variable = 6:\n             ^\n  SyntaxError: invalid syntax")
-
-    expect(data == null).to.equal(false)
-    expect(data[1]).to.equal("variable")
-    expect(data[2]).to.equal("6")
+    var helpObj = help("Traceback (most recent call last):\n  File \"python\", line 2\n    if variable = 6:\n             ^\n  SyntaxError: invalid syntax")
+    //match?
+    expect(helpObj).to.not.equal(null)
+    //captured what it should capture?
+    expect(helpObj.groups).to.deep.equal(["if", "variable", "6"])
   })
+
+
+
+
+
 })
