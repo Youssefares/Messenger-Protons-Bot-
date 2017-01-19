9c83962cbfe6425a5eb96deb98f7dab5b951c287
Youssefares <Youssefares@ymail.com>
2017-01-20 01:00:47 +0200

    added more errors & more tests

--- helpers/python-help.js
+++ helpers/python-help.js
@@ -20,13 +20,29 @@ function help(lines){
   SyntaxError: invalid syntax
   */
 
-  var result = /(if|while)\s+?(\w+)\s*=\s*(.+)\s*?:\s+.*\s+SyntaxError: invalid syntax/.exec(lines)
+  var result = /(if|while)\s+(\w+)\s*=(?!=)\s*(.+)\s*:\s+.*\s+SyntaxError: invalid syntax/.exec(lines)
   if(result){
     let response = String.raw`Looks like you forgot to use "==" to compare "${result[2]}" and "${result[3]}" inside your ${result[1]} condition.` +
     '\nRemember a single = sign means assignment:\nx == 1 is a literal of type bool that is either true or false.\nx = 1 is a statement that puts 1 inside of x.'
 
     return { response: response, groups: groups(result)}
   }
+
+
+  /*
+  Traceback (most recent call last):
+  File "python", line 2
+    while variable == 6
+                      ^
+  SyntaxError: invalid syntax
+  */
+  result = /(if|while)\s+\w+\s*={2,3}(?!=)\s*[^:]+\s*\s+.*\s+SyntaxError: invalid syntax/.exec(lines)
+  if(result){
+    let response = String.raw`Looks like you \'re missing a colon ":" at the end of your ${result[1]} condition.`
+    return {response: response, groups: groups(result)}
+  }
+
+
 
   //no matches?
   return null
--- tests/python-help-test.js
+++ tests/python-help-test.js
@@ -4,7 +4,7 @@ let chai = require('chai')
 let expect = chai.expect
 let help = require('../helpers/python-help')
 
-describe('helpers/python-help', function(){
+describe('helpers/python-help: help()', function(){
   /*
   ________________________________
   Format:
@@ -24,7 +24,7 @@ describe('helpers/python-help', function(){
            ^
   SyntaxError: invalid syntax
   */
-  it('exec(regex) should not return null & should capture the two values being compared', function(){
+  it('should not return null & should capture the two values being compared + if or while', function(){
     var helpObj = help("Traceback (most recent call last):\n  File \"python\", line 2\n    if variable = 6:\n             ^\n  SyntaxError: invalid syntax")
     //match?
     expect(helpObj).to.not.equal(null)
@@ -32,8 +32,31 @@ describe('helpers/python-help', function(){
     expect(helpObj.groups).to.deep.equal(["if", "variable", "6"])
   })
 
+  /*
+  Traceback (most recent call last):
+  File "python", line 2
+    while variable == 6
+                      ^
+  SyntaxError: invalid syntax
+  */
 
+  it('should not return null & should capture while', function(){
+    var helpObj = help("Traceback (most recent call last):\n  File \"python\", line 2\n    while variable == 6\n                      ^\nSyntaxError: invalid syntax")
+    expect(helpObj).to.not.equal(null)
+    expect(helpObj.groups).to.deep.equal(["while"])
+  })
 
-
+  /*
+  Traceback (most recent call last):
+  File "python", line 4
+    if x == y+1
+              ^
+  SyntaxError: invalid syntax
+  */
+  it('should not return null & should capture if',function(){
+    var helpObj = help("Traceback (most recent call last):\n  File \"python\", line 4\n    if x == y+1\n              ^\nSyntaxError: invalid syntax")
+    expect(helpObj).to.not.equal(null)
+    expect(helpObj.groups).to.deep.equal(["if"])
+  })
 
 })
