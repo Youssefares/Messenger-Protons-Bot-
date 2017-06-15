9016f7a675feccbf3ebec3b7ca0bbb6a869f8c19
Youssefares <Youssefares@ymail.com>
2017-06-15 22:57:35 +0200

    factored error-help out to python-error-helper api

--- helpers/about-error.js
+++ helpers/about-error.js
@@ -1,15 +1,25 @@
 //action(s) called by wit.ai when the user has an error problem. (When the entity 'error' is set to be exact.)
 'use strict'
-
-const help = require('./python-help')
+const request = require('request');
 
 //gets error help for when an error type is specified
 exports.aboutError = ({sessionId, context, text, entities}) => {
+
   return new Promise(function(resolve,reject){
-    var about_error = help(text).response
-    if(about_error){
-      context.about_error = about_error
-    }
-    return resolve(context)
+    request.post({
+      url:     'https://python-error-helper.herokuapp.com/error-help',
+      form:    { error_message: text}
+    }, function(error, response, body){
+      if(error) return reject(error)
+      //if I don't have response I'll do sth else.
+      else if(body == ''){
+        context.about_error = "something_I_know_from_error_type"
+      }
+      else{
+        var helpObj = JSON.parse(body)
+        context.about_error = helpObj.response
+      }
+      return resolve(context)
+    });
   })
 }
